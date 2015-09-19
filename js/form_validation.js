e7a9df8e11bab3bf1683ddf3c29054ac01d0bd95
4er <baidarovden@gmail.com>
2015-09-20 00:04:10 +0300

    Fix bugs

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,77 +1,68 @@
 (function () {
 
-    function is_valid_form() {
-        if (review_name.value != "" && review_text.value != "") {
-            label_form.style.display = "none";
-            return true;
-        } else {
-            label_form.style.display = "inline-block";
-        }
-    }
-
-    function get_checked_mark() {
-        var mark_list = document.getElementsByName('review-mark');
-
-        for (var i = 0; i < mark_list.length; i++) {
-            if (mark_list[i].checked) {
-                return i;
-            }
-        }
-
-    }
-
-    function set_cookies() {
-        var mark = docCookies.getItem('mark');
-        review_name.value = docCookies.getItem('name');
-
-        if (mark) {
-            document.getElementsByName('review-mark')[mark].checked = true;
-        }
-        is_valid_form();
-    }
+    "use strict";
 
     var form = document.forms[1];
 
-    var review_name = form['review-name'];
-    var review_text = form['review-text'];
+    var review_name = form["review-name"];
+    var review_text = form["review-text"];
 
-    var label_form = document.getElementsByClassName('review-form-control')[1];
-    var label_name = document.getElementsByClassName('review-fields-label review-fields-name')[0];
-    var label_text = document.getElementsByClassName('review-fields-label review-fields-text')[0];
-    var cookies_age = new Date().getTime() + (new Date().getTime() - new Date('27 December 1990').getTime());
+    var label_form = document.getElementsByClassName('review-fields')[0];
+    var label_name = label_form.getElementsByClassName('review-fields-name')[0];
+    var label_text = label_form.getElementsByClassName("review-fields-text")[0];
+    var time = new Date().getTime();
 
-    set_cookies();
-
-
-    review_name.onchange = function (event) {
-        if (review_name.value != "") {
+    function check_validation() {
+        if (review_name.value !== "") {
             label_name.style.display = "none";
         } else {
-            label_name.style.display = "inline";
+            label_name.style.display = "";
         }
 
-        is_valid_form();
+        if (review_text.value !== "") {
+            label_text.style.display = "none";
+        } else {
+            label_text.style.display = "";
+        }
+
+        if (review_name.value !== "" && review_text.value !== "") {
+            label_form.style.display = "none";
+            return true;
+        }
+
+        label_form.style.display = "";
+        return false;
+    }
+
+    function update_fields_from_cookies() {
+        var mark = docCookies.getItem("mark");
+        review_name.value = docCookies.getItem("name");
+        form['review-mark'].value = docCookies.getItem("mark");
+
+        check_validation();
+    }
+
+    review_name.onchange = function (event) {
+        check_validation();
     };
 
     review_text.onchange = function (event) {
-        if (review_text.value != "") {
-            label_text.style.display = "none";
-        } else {
-            label_text.style.display = "inline";
-        }
-        is_valid_form();
+        check_validation();
     };
 
     form.onsubmit = function (event) {
         event.preventDefault();
 
-        if (is_valid_form()) {
+        if (check_validation()) {
+            var cookies_age = time + (time - new Date("27 December 1990").getTime());
 
-            docCookies.setItem('name', review_name.value, cookies_age);
-            docCookies.setItem('mark', get_checked_mark(), cookies_age);
+            docCookies.setItem("name", review_name.value, cookies_age);
+            docCookies.setItem("mark", form['review-mark'].value, cookies_age);
 
             form.submit();
         }
     }
+
+    update_fields_from_cookies();
 
 })();
