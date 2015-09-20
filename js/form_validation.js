959d3fcc68e49f7287e3251b9273f5664866ef40
4er <baidarovden@gmail.com>
2015-09-20 17:31:43 +0300

    Last fix

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,16 +1,16 @@
-(function () {
+"use strict";
 
-    "use strict";
+(function () {
 
     var form = document.forms[1];
 
     var review_name = form["review-name"];
     var review_text = form["review-text"];
+    var review_mark = form['review-mark'];
 
     var label_form = document.getElementsByClassName('review-fields')[0];
     var label_name = label_form.getElementsByClassName('review-fields-name')[0];
     var label_text = label_form.getElementsByClassName("review-fields-text")[0];
-    var time = new Date().getTime();
 
     function check_validation() {
         if (review_name.value !== "") {
@@ -37,7 +37,7 @@
     function update_fields_from_cookies() {
         var mark = docCookies.getItem("mark");
         review_name.value = docCookies.getItem("name");
-        form['review-mark'].value = docCookies.getItem("mark");
+        review_mark.value = docCookies.getItem("mark");
 
         check_validation();
     }
@@ -54,10 +54,11 @@
         event.preventDefault();
 
         if (check_validation()) {
+            var time = new Date().getTime();
             var cookies_age = time + (time - new Date("27 December 1990").getTime());
 
             docCookies.setItem("name", review_name.value, cookies_age);
-            docCookies.setItem("mark", form['review-mark'].value, cookies_age);
+            docCookies.setItem("mark", review_mark.value, cookies_age);
 
             form.submit();
         }
