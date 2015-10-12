68a906559e6101be1606ace12ccc724782810a30
4er <baidarovden@gmail.com>
2015-10-12 14:01:43 +0300

    task1 done

--- js/game_demo.js
+++ js/game_demo.js
@@ -1,12 +1,31 @@
 'use strict';
 
-(function() {
+(function () {
   var clouds = document.querySelector('.header-clouds');
+  var cloudsVisibility = true;
   clouds.style.left = '0px';
 
-  window.addEventListener('scroll', function() {
+  function checkVisibleClouds() {
     var cloudsVisiblePart = clouds.getBoundingClientRect().bottom;
     if (cloudsVisiblePart > 0) {
+      window.dispatchEvent(new CustomEvent('cloudsHidden'));
+    }
+  }
+
+  function initScroll() {
+    var checkTimeout;
+    window.addEventListener('scroll', function () {
+      clearTimeout(checkTimeout);
+      checkTimeout = setTimeout(checkVisibleClouds, 100);
+    });
+
+    window.addEventListener('cloudsHidden', function () {
+      cloudsVisibility = false;
+    });
+  }
+
+  window.addEventListener('scroll', function () {
+    if (cloudsVisibility) {
       var oldLeft = parseInt(clouds.style.left, 10);
       clouds.style.left = oldLeft + 10 + 'px';
     }
