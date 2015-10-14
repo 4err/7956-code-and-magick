0c53af5d1105d6bf8cd89e862dff0f0194cba812
4er <baidarovden@gmail.com>
2015-10-14 22:02:53 +0300

    fix

--- js/game_demo.js
+++ js/game_demo.js
@@ -4,29 +4,29 @@
   var clouds = document.querySelector('.header-clouds');
   var cloudsVisibility = true;
   clouds.style.backgroundPositionX = '50%';
-  var left = 50;
 
   function checkVisibleClouds() {
+    console.log('Проверка!');
     var cloudsVisiblePart = clouds.getBoundingClientRect().bottom;
     if (cloudsVisiblePart < 0) {
       window.dispatchEvent(new CustomEvent('cloudsHidden'));
     } else {
       window.dispatchEvent(new CustomEvent('cloudsShow'));
     }
-
   }
 
-  var checkTimeout;
+  function throttle(method, time) {
+    var checkTimeout;
+    clearTimeout(checkTimeout);
+    checkTimeout = setTimeout(method.call(this), time);
+  }
 
   window.addEventListener('scroll', function(evt) {
-    console.log(evt);
     if (cloudsVisibility) {
-      clouds.style.backgroundPositionX = left + 5 + '%';
-      left += 5;
+      clouds.style.backgroundPositionX = document.body.scrollTop + 'px';
     }
 
-    clearTimeout(checkTimeout);
-    checkTimeout = setTimeout(checkVisibleClouds, 100);
+    throttle(checkVisibleClouds, 1000);
   });
 
   window.addEventListener('cloudsHidden', function() {
