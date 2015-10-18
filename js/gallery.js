9b8c8e52f664d539e0f1368f82db9271d9caff24
4er <baidarovden@gmail.com>
2015-10-18 22:07:34 +0300

    Gallery

--- js/gallery.js
+++ js/gallery.js
@@ -8,18 +8,68 @@
     'RIGHT': 39
   };
 
-  var photoGallery = document.querySelector('.photogallery');
-  var galleryOverlay = document.querySelector('.overlay-gallery');
-  var closeButton = galleryOverlay.querySelector('.overlay-gallery-close');
-  var leftButton = galleryOverlay.querySelector('.overlay-gallery-control-left');
-  var rightButton = galleryOverlay.querySelector('.overlay-gallery-control-right');
+  var Gallery = function() {
+    this._galleryOverlay = document.querySelector('.overlay-gallery');
+    this._closeButton = galleryOverlay.querySelector('.overlay-gallery-close');
+    this._leftButton = galleryOverlay.querySelector('.overlay-gallery-control-left');
+    this._rightButton = galleryOverlay.querySelector('.overlay-gallery-control-right');
+    this._pictureElement = this._galleryOverlay.querySelector('.overlay-gallery-preview');
 
-  function hideGallery() {
-    galleryOverlay.classList.add('invisible');
-    closeButton.removeEventListener('click', closeHandler);
-    leftButton.removeEventListener('click', leftButtonHandler);
-    rightButton.removeEventListener('click', rightButtonHandler);
-    document.body.removeEventListener('keydown', keyHandler);
+    this._currentPhoto = 0;
+    this._photos = [];
+
+    this._onCloseClick = this._onCloseClick.bind(this);
+    this._onLeftButtonClick = this._onLeftButtonClick.bind(this);
+    this._onRightButtonClick = this._onRightButtonClick.bind(this);
+    this._onKeyDown = this._onKeyDown.bind(this);
+  }
+
+  var photoGallery = document.querySelector('.photogallery');
+
+  Gallery.prototype.showGallery = function() {
+    this._galleryOverlay.classList.remove('invisible');
+    this._closeButton.addEventListener('click', this._onCloseClick);
+    this._leftButton.addEventListener('click', this._onLeftButtonClick);
+    this._rightButton.addEventListener('click', this._onRightButtonClickn);
+    document.body.addEventListener('keydown', this._onKeyDown);
+
+    this._showCurrentPhoto();
+  }
+
+  Gallery.prototype.hideGallery = function() {
+    this._galleryOverlay.classList.add('invisible');
+    this._closeButton.removeEventListener('click', this._onCloseClick);
+    this._leftButton.removeEventListener('click', this._onLeftButtonClick);
+    this._rightButton.removeEventListener('click', this._onRightButtonClickn);
+    document.body.removeEventListener('keydown', this._onKeyDown);
+
+    this._photos = [];
+    this._currentPhoto = 0;
+  }
+
+  Gallery.prototype.showCurrentPhoto = function() {
+    this._pictureElement.innerHTML = '';
+
+    var image = new Image();
+    image.src = this._photos[this._currentPhoto];
+    image.onload = function() {
+      this._pictureElement.appendChild(image);
+    }.bind(this);
+  }
+
+  Gallery.prototype._onCloseClick = function(event) {
+    event.preventDefault();
+    this.hideGallery();
+  }
+
+  Gallery.prototype._onLeftButtonClick = function(event) {
+    event.preventDefault();
+    showPrevImage();
+  }
+
+  Gallery.prototype._onRightButtonClick = function(event) {
+    event.preventDefault();
+    showNextImage();
   }
 
   function showNextImage() {
@@ -30,29 +80,6 @@
     console.log('Show prev image');
   }
 
-  function closeHandler(evt) {
-    evt.preventDefault();
-    hideGallery();
-  }
-
-  function leftButtonHandler(evt) {
-    evt.preventDefault();
-    showPrevImage();
-  }
-
-  function rightButtonHandler(evt) {
-    evt.preventDefault();
-    showNextImage();
-  }
-
-  function showGallery() {
-    galleryOverlay.classList.remove('invisible');
-    closeButton.addEventListener('click', closeHandler);
-    leftButton.addEventListener('click', leftButtonHandler);
-    rightButton.addEventListener('click', rightButtonHandler);
-    document.body.addEventListener('keydown', keyHandler);
-  }
-
   photoGallery.addEventListener('click', function(evt) {
     evt.preventDefault();
     if (evt.target.tagName === 'IMG') {
@@ -60,8 +87,8 @@
     }
   });
 
-  function keyHandler(evt) {
-    switch (evt.keyCode) {
+ Gallery.prototype._onKeyDown = function(event) {
+    switch (event.keyCode) {
       case Key.LEFT:
         showPrevImage();
         break;
@@ -70,8 +97,13 @@
         break;
       case Key.ESC:
       default:
-        hideGallery();
+        this.hideGallery();
         break;
     }
-  }
+  };
+
+ Gallery.prototype.setPhotos = function(photos) {
+    this._photos = photos;
+  };
+
 })();
